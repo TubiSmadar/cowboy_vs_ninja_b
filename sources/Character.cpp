ef7463248bd12e8f8271d720b7d9161004bf3fa8
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-05-22 14:53:57 +0300

    Update Character.cpp

--- sources/Character.cpp
+++ sources/Character.cpp
@@ -7,44 +7,64 @@ using namespace std;
 namespace ariel
 {
     Character::Character(string name, Point point) : name(name), point(point){
-    
+        this->inTeam = false;
     }
     Character::Character(string name, Point point, int hitpoints) : name(name), point(point), hitpoints(hitpoints)
     {
+        this->inTeam = false;
     }
 
     bool Character::isAlive()
     {
-        return true;
-    }
-    double Character::distance(Character *other)
-    {
-        return 0.0;
+        return this->hitpoints > 0;
     }
     void Character::hit(int damage)
     {
+        if (damage < 0)
+        {
+            throw std::invalid_argument("can't heal in this game!");
+        }
+        this->hitpoints = this->hitpoints - damage;
     }
     string Character::getName()
     {
-        return "";
+        return this->name;
     }
     Point Character::getLocation()
     {
-        return Point(0.0, 0.0);
+        return this->point;
+    }
+    void Character::setLocation(Point location){
+        this->point = location;
     }
     string Character::print()
     {
-        return "";
+        std::string info = "Name: " + name + "\n";
+        info += "Hitpoints: " + std::to_string(hitpoints) + "\n";
+        info += "Point: ";
+        point.print(this->point);
+        info += "\n";
+        return info;
     }
 
     int Character::getHitpoints()
     {
-        return 0;
+        return this->hitpoints;
+    }
+    
+    void Character::setHitpoints(int hitpoints){
+        this->hitpoints = hitpoints;
+    }
+    
+    double Character::distance(Character *other){
+        return this->point.distance(other->getLocation());
+    }
+    void Character::setInTeam(bool exp) {
+        this->inTeam = exp;
+    }
+    bool Character::getInTeam(){
+        return this->inTeam;
     }
 
-    void Character::moveTowards(Point point, double distance)
-    {
-
-    }
 
 }
