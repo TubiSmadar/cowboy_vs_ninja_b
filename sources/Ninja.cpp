34d752f51834da9d0bc57bb2aa0454c5a745540b
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-05-22 14:58:07 +0300

    Update Ninja.cpp

--- sources/Ninja.cpp
+++ sources/Ninja.cpp
@@ -13,11 +13,25 @@ using namespace std;
     }
 
     void Ninja::move(Character* enemy){
-
+        Point dest = this->getLocation().moveTowards(this->getLocation(), enemy->getLocation(),this->speed);
+        this->setLocation(dest);
     }
 
     void Ninja::slash(Character* enemy){
-        
+        if (enemy == this)
+        {
+            throw std::runtime_error("Call someone if you need mental help!");
+        }
+        if (!(this->isAlive())){
+            throw std::runtime_error("Ninja is dead!");
+        }
+        if (!(enemy->isAlive())){
+            throw std::runtime_error("Enemy is dead!");
+        }
+        if (this->isAlive() && this->getLocation().distance(enemy->getLocation()) < 1)
+        {
+            enemy->hit(40);
+        }
     }
     
 };
