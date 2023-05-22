a03fedef8726f6f17ef49e2484c194d1a4156ee0
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-05-22 14:58:30 +0300

    Update Point.cpp

--- sources/Point.cpp
+++ sources/Point.cpp
@@ -1,20 +1,45 @@
         #include "Point.hpp"
 
         namespace ariel {
-
+        Point::Point(double xCoordinate, double yCoordinate) : yCoordinate(yCoordinate), xCoordinate(xCoordinate){
         
-        Point::Point(double xCoordinate, double yCoordinate){
 
         }
-
-        double Point::distance (Point a){
-                return 0;        }
-
-        void Point::print(Point a){
-
+        double Point::getX(){return this->xCoordinate;}
+        double Point::getY(){return this->yCoordinate;}
+        double Point::distance(Point a)
+        {
+        double deltaX = a.getX() - xCoordinate;
+        double deltaY = a.getY() - yCoordinate;
+        return std::sqrt(deltaX * deltaX + deltaY * deltaY);
         }
 
-        Point Point::moveTowards(Point a, Point b, double distance){
-                return Point(0,0);
+
+        std::string Point::print(Point a){
+                std::string info = "(" + std::to_string(xCoordinate) + "," + std::to_string(yCoordinate) + ")";
+                return info;
+        }
+
+        Point Point::moveTowards(Point source, Point dest, double distance){
+        if(distance < 0) 
+                throw std::invalid_argument("Distance can not be negative!");
+                std::cout<<"old:"<<distance<<std::endl;
+
+        double powX = dest.xCoordinate - source.xCoordinate;
+        double powY = dest.yCoordinate - source.yCoordinate;
+        double newDistance = source.distance(dest);
+        if(newDistance <= distance) {
+                std::cout<<"new:"<<newDistance<<std::endl;
+                std::cout<<"old:"<<distance<<std::endl;
+                return dest;}
+        else
+        {
+            double flag = distance / newDistance;
+            double newX = source.xCoordinate + (powX * flag);
+            double newY = source.yCoordinate + (powY * flag);
+                std::cout<<"old:"<<distance<<std::endl;
+
+            return Point(newX, newY);
+        }
         }
         }
