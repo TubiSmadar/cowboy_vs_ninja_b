fb2419f7a9e0c640ca75c6a95ce2ac40f92a91f6
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-05-22 14:57:02 +0300

    Update Character.hpp

--- sources/Character.hpp
+++ sources/Character.hpp
@@ -1,21 +1,35 @@
-#ifndef COWBOY_HPP
-#define COWBOY_HPP
-#include "Character.hpp"
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
 
-
+#include <iostream>
+#include "Point.hpp"
 
 using namespace std;
-namespace ariel{
-    class Cowboy : public Character{
-    private:
-    int bullets;
 
+namespace ariel
+{
+    class Character
+    {
+    private:
+        Point point;
+        int hitpoints;
+        std::string name;
+        bool inTeam;
     public:
-    Cowboy(std::string name, Point point);
-    void shoot(Character* enemy);
-    bool hasboolets();
-    void reload();
-};
+        Character(string name, Point point);
+        Character(string name, Point point, int hitpoints);
+        bool isAlive();
+        double distance(Character *other);
+        void hit(int damage);
+        string getName();
+        Point getLocation();
+        void setLocation(Point location);
+        virtual string print();
+        int getHitpoints();
+        void setHitpoints(int hitpoints);
+        void setInTeam(bool exp);
+        bool getInTeam();
+    };
 }
 
-#endif
+#endif // CHARACTER_HPP
