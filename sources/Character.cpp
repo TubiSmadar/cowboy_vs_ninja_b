ede3dcd3af52a7f973665332c657058d06bd4268
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-05-22 14:54:14 +0300

    Update Character.cpp

--- sources/Character.cpp
+++ sources/Character.cpp
@@ -1,70 +1,35 @@
-#include <iostream>
-#include <cmath>
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
 
-#include "Character.hpp"
+#include <iostream>
+#include "Point.hpp"
+
 using namespace std;
 
 namespace ariel
 {
-    Character::Character(string name, Point point) : name(name), point(point){
-        this->inTeam = false;
-    }
-    Character::Character(string name, Point point, int hitpoints) : name(name), point(point), hitpoints(hitpoints)
+    class Character
     {
-        this->inTeam = false;
-    }
-
-    bool Character::isAlive()
-    {
-        return this->hitpoints > 0;
-    }
-    void Character::hit(int damage)
-    {
-        if (damage < 0)
-        {
-            throw std::invalid_argument("can't heal in this game!");
-        }
-        this->hitpoints = this->hitpoints - damage;
-    }
-    string Character::getName()
-    {
-        return this->name;
-    }
-    Point Character::getLocation()
-    {
-        return this->point;
-    }
-    void Character::setLocation(Point location){
-        this->point = location;
-    }
-    string Character::print()
-    {
-        std::string info = "Name: " + name + "\n";
-        info += "Hitpoints: " + std::to_string(hitpoints) + "\n";
-        info += "Point: ";
-        point.print(this->point);
-        info += "\n";
-        return info;
-    }
-
-    int Character::getHitpoints()
-    {
-        return this->hitpoints;
-    }
-    
-    void Character::setHitpoints(int hitpoints){
-        this->hitpoints = hitpoints;
-    }
-    
-    double Character::distance(Character *other){
-        return this->point.distance(other->getLocation());
-    }
-    void Character::setInTeam(bool exp) {
-        this->inTeam = exp;
-    }
-    bool Character::getInTeam(){
-        return this->inTeam;
-    }
-
-
+    private:
+        Point point;
+        int hitpoints;
+        std::string name;
+        bool inTeam;
+    public:
+        Character(string name, Point point);
+        Character(string name, Point point, int hitpoints);
+        bool isAlive();
+        double distance(Character *other);
+        void hit(int damage);
+        string getName();
+        Point getLocation();
+        void setLocation(Point location);
+        virtual string print();
+        int getHitpoints();
+        void setHitpoints(int hitpoints);
+        void setInTeam(bool exp);
+        bool getInTeam();
+    };
 }
+
+#endif // CHARACTER_HPP
