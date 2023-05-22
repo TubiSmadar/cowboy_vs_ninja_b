dc9fa6b744bca46d23b60b72c44664682066ae12
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-05-22 17:29:47 +0300

    Update Point.cpp

--- sources/Point.cpp
+++ sources/Point.cpp
@@ -23,21 +23,17 @@
         Point Point::moveTowards(Point source, Point dest, double distance){
         if(distance < 0) 
                 throw std::invalid_argument("Distance can not be negative!");
-                std::cout<<"old:"<<distance<<std::endl;
 
         double powX = dest.xCoordinate - source.xCoordinate;
         double powY = dest.yCoordinate - source.yCoordinate;
         double newDistance = source.distance(dest);
         if(newDistance <= distance) {
-                std::cout<<"new:"<<newDistance<<std::endl;
-                std::cout<<"old:"<<distance<<std::endl;
                 return dest;}
         else
         {
             double flag = distance / newDistance;
             double newX = source.xCoordinate + (powX * flag);
             double newY = source.yCoordinate + (powY * flag);
-                std::cout<<"old:"<<distance<<std::endl;
 
             return Point(newX, newY);
         }
