f67294aeefc218f1d584b2df49a93d01a8445486
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-05-24 17:20:46 +0300

    Update Point.hpp

--- sources/Point.hpp
+++ sources/Point.hpp
@@ -10,6 +10,11 @@ namespace ariel {
         double yCoordinate;
 
         public:
+        virtual ~Point() = default;
+        Point(const Point&) = default;
+        Point& operator=(const Point&) = default;
+        Point(Point&&) = default;
+        Point& operator=(Point&&) = default;
         Point(double xCoordinate, double yCoordinate);
         double getX();
         double getY();
